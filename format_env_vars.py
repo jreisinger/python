a1c61a84a0a1ce462609b3eaf52752189efbe84f
Jozef Reisinger <jreisinger@users.noreply.github.com>
2017-11-22 09:00:29 +0100

    Update format_env_vars.py

--- format_env_vars.py
+++ format_env_vars.py
@@ -4,10 +4,10 @@ import os
 
 max_key_len = 0
 
-for k,v in os.environ.items():
-    if len(k) > max_key_len:
-        max_key_len = len(k)
-        continue
+for k in os.environ.keys():
+    length = len(k)
+    if length > max_key_len:
+        max_key_len = length
 
 for k,v in sorted(os.environ.items()):
-    print('{:{align}{width}} => {}'.format(k,v,align='<',width=max_key_len))
+    print('{:{align}{width}} --> {}'.format(k,v,align='<',width=max_key_len))
