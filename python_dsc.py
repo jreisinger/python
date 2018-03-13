aba3c4a19a5cc5d620947804e51ec93cb5dac749
Jozef Reisinger <jozef.reisinger@telekom.com>
2018-03-13 15:22:20 +0100

    start python data structure cookbook

---
+++ python_dsc.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+LIST_OF_TUPLES = [
+    ('one',   1),
+    ('two',   2),
+    ('three', 3),
+]
+
+LIST_OF_DICTIONARIES = [
+    {'one':   1},
+    {'two':   2},
+    {'three': 3},
+]
+
+def print_lot(data, tmpl):
+    for name, value in data:
+        print(tmpl.format( name, value ))
+
+def print_lod(data, tmpl):
+    for dict_item in data:
+        for key in dict_item:
+            print(tmpl.format( key, dict_item[key] ))
+
+def main():
+    tmpl = '{:<10} {}'
+    print_lot(LIST_OF_TUPLES, tmpl)
+    print_lod(LIST_OF_DICTIONARIES, tmpl)
+
+if __name__ == '__main__':
+    main()
