7ac07156d52501d8eeeb3d37133fc4d7155d9324
Jozef Reisinger <jozef.reisinger@telekom.com>
2018-03-28 09:21:02 +0200

    update file sets.py

--- sets.py
+++ sets.py
@@ -10,10 +10,11 @@ drinks = {
         'screwdriver': {'orange juice', 'vodka'},
         }
 
+print("Drinks with vodka but neither cream nor vermouth:")
 for name, contents in drinks.items():
-    # Drinks with vodka but neither cream nor vermouth.
     # & -- set intersection operator -- returns a set, which contains all the
     # items that appear in both lists that you compare.
     if 'vodka' in contents and not contents & {'vermouth', 'cream'}:
     #if 'vodka' in contents and not ('cream' in contents or 'vermouth' in contents):
-        print( "{}: {}".format( name, ", ".join(contents) ) )
+        print( "- {}: {}".format( name, ", ".join(contents) ) )
+
