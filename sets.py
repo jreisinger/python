653a49d039c0ca1a0a3a9e74a52c4647e80ad642
Jozef Reisinger <jozef.reisinger@telekom.com>
2018-03-28 09:09:00 +0200

    update file sets.py

--- sets.py
+++ sets.py
@@ -12,6 +12,8 @@ drinks = {
 
 for name, contents in drinks.items():
     # Drinks with vodka but neither cream nor vermouth.
-    #if 'vodka' in contents and not ('cream' in contents or 'vermouth' in contents):
+    # & -- set intersection operator -- returns a set, which contains all the
+    # items that appear in both lists that you compare.
     if 'vodka' in contents and not contents & {'vermouth', 'cream'}:
+    #if 'vodka' in contents and not ('cream' in contents or 'vermouth' in contents):
         print( "{}: {}".format( name, ", ".join(contents) ) )
