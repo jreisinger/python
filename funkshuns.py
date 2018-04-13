760351ae83f8f0d702292d1ade12c1dde177e0e4
Jozef Reisinger <jozef.reisinger@telekom.com>
2018-04-13 09:37:00 +0200

    gather positional args

--- funkshuns.py
+++ funkshuns.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python3
 
 # Default argument values are calculated when the function is *defined*, not
-# when it is run! Talking about result=[] ... :-)
-def accumulate(arg, result=[], keep_state=True):
+# when it is run! Talking about result=[] ...
+# ---
+# Asterisk (*) does not mean a pointer (Python has no pointers). It groups a
+# variable number of positional arguments into a tuple of paramter values.
+# Talking about *args ... :-)
+def accumulate(*args, result=[], keep_state=True):
     if not keep_state:
         result = [] # clear the list
-    result.append(arg)
+    for a in args:
+        result.append(a)
     print(result)
 
 # keep state of result between function calls
@@ -15,3 +20,4 @@ accumulate('a')
 
 # clear state of result
 accumulate('a', keep_state=False)
+accumulate('b', 'b', keep_state=False)
