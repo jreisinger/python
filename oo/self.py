ebe53e010dd291228ae5c897c9fb4248b75288cb
Jozef Reisinger <jreisinger@users.noreply.github.com>
2018-05-03 08:55:34 +0200

    Update self.py

--- oo/self.py
+++ oo/self.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
 
 class Car():
-    # (1) Why we need this "self" crap? See below ... :-)
+    # (A) Why we need this "self" crap? See (A) below ... :-)
     def exclaim(self):
         print("I'm a car!")
 
     def __init__(self, name):
-        # (2) access "name" attribute inside of class (generic object)
+        # (B) access "name" attribute inside of class (generic object)
         self.name = name
 
 car = Car(name="Mater")
 car.exclaim()
-# (1) -> this is what Python actually does
+# (A) -> this is what Python actually does
 Car.exclaim(car)
 
-# (2) access "name" attribute outside of class (specifi object)
+# (B) access "name" attribute outside of class (specific object)
 print(car.name)
