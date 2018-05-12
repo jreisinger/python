60b37d1d3be49f73ac51fa4f46915561789cf432
Jozef Reisinger <jozef.reisinger@telekom.com>
2018-05-12 15:39:03 +0200

    Write a file by chunks

--- data-persistence/rw-files.py
+++ data-persistence/rw-files.py
@@ -5,23 +5,36 @@ Do not meddle in the affairs of wizzards,
 for you are crunchy and good with ketchup.\
 """
 
-print("The text has", len(text), "bytes")
+print("--> The text has", len(text), "bytes")
 
 filename = "/tmp/wizz.txt"
 
-# Write a text file...
+print("--> Write a text file...")
 fileobj = open(filename, "wt") # "t" is default. 2nd option is "b"inary
 bytes_written = fileobj.write(text)
 print(bytes_written, "bytes written to", filename)
 fileobj.close()
 
-# Read a text file all lines at once...
+print("--> Read a text file all lines at once...")
 fileobj = open(filename, "r") # "r" is default. Other opts: "w", "x", "a"
 slurped = fileobj.read()
 print(slurped)
 fileobj.close()
 
-# Read a text file line by line...
+print("--> Read a text file line by line...")
 lines = open(filename).readlines() # readlines() is optional
 for line in lines:
-    print(line)
+    print(line, end="")
+print()
+
+print("--> Write a file by chunks...")
+fout = open(filename, "w")
+size = len(text)
+offset = 0
+chunk = 10 # bytes
+while True:
+    if offset > size:
+        break
+    written = fout.write(text[offset:offset+chunk])
+    print("Written", written, "bytes")
+    offset += chunk
