d21b9cd3f545f67c4d0644c5bb657ce37f03b76c
Jozef Reisinger <jozef.reisinger@telekom.com>
2018-05-15 20:55:56 +0200

    update files parallel-processing/01-fork1.py, parallel-processing/02-fork-count.py

---
+++ parallel-processing/01-fork1.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+"forks child processes until you type 'q' (Programming Python 4th, p. 179)"
+
+import os
+
+def child():
+    print('Hello from child', os.getpid())
+    os._exit(0) # else goes back to parent loop -> it's a copy of the orig. proc
+
+def parent():
+    while True:
+        newpid = os.fork()
+        if newpid == 0:
+            child()
+        else:
+            print('Hello from parent', os.getpid(), newpid)
+        if input() == 'q': break
+
+parent()
