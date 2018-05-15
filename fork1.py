f2ba6fcaf4b7a4430e38db882807082a6366a440
Jozef Reisinger <jozef.reisinger@telekom.com>
2018-05-15 20:37:55 +0200

    update file fork1.py

--- fork1.py
+++ fork1.py
@@ -1,14 +1,12 @@
 #!/usr/bin/env python
 
-""" forks child processes until you type 'q'
-    Programming Python 4th, p. 179 """
-
+"forks child processes until you type 'q' (Programming Python 4th, p. 179)"
 
 import os
 
 def child():
     print('Hello from child', os.getpid())
-    os._exit(0)  # else goes back to parent loop
+    os._exit(0) # else goes back to parent loop -> it's a copy of the orig. proc
 
 def parent():
     while True:
