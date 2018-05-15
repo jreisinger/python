083d26f1738d573133acaef4a7f05675c41f7a0b
Jozef Reisinger <jozef.reisinger@telekom.com>
2018-05-15 21:02:35 +0200

    update file parallel-processing/03-fork-exec.py

---
+++ parallel-processing/03-fork-exec.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"starts programs unitl you type 'q'"
+
+import os
+
+parm = 0
+while True:
+    parm += 1
+    pid = os.fork()
+    if pid == 0:                                             # copy process
+        os.execlp('python', 'python', 'child.py', str(parm)) # overlay program
+        assert False, 'error starting program'               # shouldn't return
+    else:
+        print('Child is', pid)
+        if input() == 'q': break
