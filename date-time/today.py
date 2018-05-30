fd4f1c8bcd7e01ef566a1c23406941f51a581a09
Jozef Reisinger <jozef.reisinger@telekom.com>
2018-05-30 08:55:08 +0200

    update file date-time/today.py

---
+++ date-time/today.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+""" write the current date as a string to a text file
+"""
+
+from datetime import date
+
+today       = date.today()
+today_str   = today.isoformat()
+
+fout = open("today.txt", "w")
+print(today_str, file=fout)
