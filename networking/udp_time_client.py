299ee9ee8390547637df876a8aec31295257c150
Jozef Reisinger <jozef.reisinger@telekom.com>
2018-06-14 08:19:55 +0200

    update files networking/udp_time_client.py, networking/udp_time_server.py

--- networking/udp_time_client.py
+++ networking/udp_time_client.py
@@ -9,9 +9,10 @@ max_size    = 4096
 
 client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
+
 while True:
     sleep(5)
-    client.sendto(b'time', addr)
+    client.sendto(b'time', addr) # a bytes-like object is required, not 'str'
     data, server_addr = client.recvfrom(max_size)
     print('Client read', data)
 
