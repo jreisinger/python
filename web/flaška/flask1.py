06aa4238a67728adc2ce0a36c2c5e9b954e331e3
Jozef Reisinger <jozef.reisinger@telekom.com>
2018-07-24 09:39:09 +0200

    update file "web/fla\305\241ka/flask1.py"

--- web/flaška/flask1.py
+++ web/flaška/flask1.py
@@ -1,16 +1,34 @@
 #!/usr/bin/env python3
 
-from flask import Flask
+## Initialization
 
-# defaults for static_folder and static_url_path is 'static'
+# Create application instance that will handle all requests from clients.
+# Requtests are passed by the web server using WSGI ("wiz-ghee") protocol.
+
+# Defaults for static_folder and static_url_path is 'static'.
+from flask import Flask
 app = Flask(__name__, static_folder='.', static_url_path='')
 
+## Routes and view functions
+
+# Define what code to run for each URL. These mappings of URLs to Python
+# functions are called routes. Functions like home() are called view functions.
+
+# Decorators are used to register functions as handler functions to be invoked
+# when certain events occur.
 @app.route('/')
+
 def home():
     return app.send_static_file('index.html')
 
-@app.route('/echo/<thingy>')
+# dynamic route
+@app.route('/echo/<string:thingy>')
 def echo(thingy):
     return thingy
 
 app.run(port=9999, debug=True)
+
+## To run this app:
+#
+# export FLASK_APP=flask1.py
+# flask run
